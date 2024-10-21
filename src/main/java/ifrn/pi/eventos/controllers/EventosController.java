d85020ee52bd07d1df405dc8dd9a8eca8278d375
Clara Beatriz Barbosa Lopes <20211204010009@ifrn.local>
2024-10-21 17:35:11 -0300

    Criando a página de formulário para cadastro do evento

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class EventosController {
 	
-	@RequestMapping (" /eventos/form")
+	@RequestMapping ("/eventos/form")
 	public String form() {
 		
 		return "formEvento";
